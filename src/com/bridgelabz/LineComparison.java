5af99d96d115950e3bb4cf6e923ed7234f5b4f60
akashp69 <108648856+akashp69@users.noreply.github.com>
2022-11-28 02:18:20 +0530

    Update LineComparison.java

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,6 +1,9 @@
 package com.bridgelabz;
 
 public class LineComparison {
+    /**
+    *This Method is Created For Calculate The Distance Of  Points
+    */
     public static void main(String[] args) {
         int x1,x2,y1,y2;
         double dis;
@@ -8,6 +11,9 @@ public class LineComparison {
         x2=2;
         y1=3;
         y2=4;
+    /**
+    *By Using Formula Calculate the Distance Of given points
+    */ 
         dis = Math.sqrt(x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1);
         System.out.println("distance" +dis);
     }
